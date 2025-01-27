21c6d8a89f7ce189855ef7492c79c64fd1568b91
francisco-24-0658 <franciscopsuero@gmail.com>
2025-01-27 18:16:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un año:");
+        int bisiesto = Convert.ToInt32(Console.ReadLine());
+
+        Esbisiesto(bisiesto);
+        
+    }
+
+    static void Esbisiesto(int anoingresado)
+    {
+        if ((anoingresado % 4 == 0 && anoingresado % 100 != 0) || (anoingresado % 400 == 0))
+        {
+            Console.WriteLine("Es bisiesto.");
+        }
+        else
+        {
+            Console.WriteLine("No es bisiesto.");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
